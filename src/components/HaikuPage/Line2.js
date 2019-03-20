4b908438c0c83f22ca428963cb17419fc2324402
Marifel Angeles <mmarifel@gmail.com>
2019-03-20 13:10:58 -0500

    add line2 component and display count on screen

--- src/components/HaikuPage/Line2.js
+++ src/components/HaikuPage/Line2.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 
-class Line1 extends Component {
+class Line2 extends Component {
 
     handleChange = (event) => {
-        console.log('in handle change', event.target.value );
+        console.log('in handle change', event.target.value);
 
         //save input to redux at all times
         // allow user to see input when leaving and returning to haiku page
         this.props.dispatch({
             type: 'SET_HAIKU',
             payload: event.target.value,
-            propertyName: "line1"
+            propertyName: "line2"
         });
         this.props.dispatch({
             type: 'SET_HAIKU',
             payload: this.new_count(event.target.value),
-            propertyName: "count1"
+            propertyName: "count2"
         })
     }
 
@@ -40,10 +40,10 @@ class Line1 extends Component {
             <div>
                 <input
                     type="text"
-                    value={this.props.haiku.line1}
+                    value={this.props.haiku.line2}
                     onChange={this.handleChange}
                 />
-                <p>{this.props.haiku.count1 ? this.props.haiku.count1 : 0 }/5</p>
+                <p>{this.props.haiku.count2 ? this.props.haiku.count2 : 0}/7</p>
             </div>
         );
     }
@@ -54,4 +54,4 @@ const mapStateToProps = (reduxState) => {
     return reduxState;
 }
 
-export default connect(mapStateToProps)(Line1);
+export default connect(mapStateToProps)(Line2);
--- src/components/HaikuPage/index.js
+++ src/components/HaikuPage/index.js
@@ -1,69 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Line1 from './Line1';
+import Line2 from './Line2';
 
 
 class HaikuPage extends Component {
 
-    state = {
-        line_2: '',
-        line_3: '',
-        count_1: 0,
-        count_2: 0,
-        count_3: 0,
-
-    }
-
-    handleChange = (line) => (event) => {
-        console.log('in handle change');
-        this.setState({
-            [line]: event.target.value,
-        });
-    }
-
-    // count the number of syllables in each line
-    countSyllables = (haikuLine) => {
-        console.log('in countSyllables');
-        let lineArray = haikuLine.split(" ");
-        let totalSyllables = 0;
-        lineArray.forEach(word => {
-            console.log('word:', word);
-            let syllables = this.new_count(word);
-            console.log('syllables', syllables);
-            totalSyllables = totalSyllables + syllables;
-        });
-        return totalSyllables;
-    }
-
-    // count number of syllables in a word
-    new_count = (word) => {
-        word = word.toLowerCase();
-        if (word.length <= 3) { return 1; }
-        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-        word = word.replace(/^y/, '');
-        return word.match(/[aeiouy]{1,2}/g).length;   
-    }
-
+    
     render() {
         return (
             <div>
-                {/* {JSON.stringify(this.state)} */}
                 <h2>Intuitive</h2>
+                <p>reducer: {JSON.stringify(this.props.haiku)}</p>
+
                 <Line1 />
-                <div>
-                    <input
-                        type="text"
-                        value={this.state.line_2}
-                        onChange={this.handleChange('line_2')}
-                    />
-                </div>
-                <div>
+                <Line2 />
+                {/* <div>
                     <input
                         type="text"
                         value={this.state.line_3}
                         onChange={this.handleChange('line_3')}
                     />
-                </div>
+                </div> */}
                 <div>
                     <button
                         // onClick={this.handleClick}
