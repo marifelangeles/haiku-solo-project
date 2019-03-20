924047f68fec402345edb7bbab24f09fa3a13f29
Marifel Angeles <mmarifel@gmail.com>
2019-03-20 10:55:02 -0500

    show 0 when input is empty

--- src/components/HaikuPage/Line1.js
+++ src/components/HaikuPage/Line1.js
@@ -4,42 +4,48 @@ import { connect } from 'react-redux';
 
 class Line1 extends Component {
 
-    state = {
-        line_1: '',
-        count: 0,
-    }
-
     handleChange = (event) => {
         console.log('in handle change', event.target.value );
-        // get syllable count across a line
-        console.log('get line count', this.new_count(this.state.line_1));
-        this.setState({
-            line_1: event.target.value,
-            count: this.new_count(this.state.line_1)
+
+        //save input to redux at all times
+        // allow user to see input when leaving and returning to haiku page
+        this.props.dispatch({
+            type: 'SET_HAIKU',
+            payload: event.target.value,
+            propertyName: "line1"
         });
+        this.props.dispatch({
+            type: 'SET_HAIKU',
+            payload: this.new_count(event.target.value),
+            propertyName: "count1"
+        })
     }
 
     // count syllables in a word
+    // code found in https://stackoverflow.com/questions/5686483/how-to-compute-number-of-syllables-in-a-word-in-javascript
     new_count = (word) => {
-        word = word.toLowerCase();
-        if (word.length <= 3) { return 1; }
-        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-        word = word.replace(/^y/, '');
-        return word.match(/[aeiouy]{1,2}/g).length;
+        if (word) {
+            word = word.toLowerCase();
+            if (word.length <= 3) { return 1; }
+            word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+            word = word.replace(/^y/, '');
+            return word.match(/[aeiouy]{1,2}/g).length;
+        }
+        return 0;
     }
 
 
     render() {
-
         return (
             <div>
-                <p>{JSON.stringify(this.state)}</p>
+                <p>reducer: {JSON.stringify(this.props.haiku)}</p>
+                <p>state: {JSON.stringify(this.state)}</p>
                 <input
                     type="text"
-                    value={this.props.haikuReducer}
+                    value={this.props.haiku.line1}
                     onChange={this.handleChange}
                 />
-                <p>{this.state.count}/5</p>
+                <p>{this.props.haiku.count1 ? this.props.haiku.count1 : 0 }/5</p>
             </div>
         );
     }
