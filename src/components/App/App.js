a0f4daccb6d08e42feb046e8b6e6ac88edf548cf
Marifel Angeles <mmarifel@gmail.com>
2019-03-21 20:32:41 -0500

    clean up nav component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -51,6 +51,7 @@ class App extends Component {
               exact path="/history"
               component={HistoryPage}
             />
+            
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
@@ -60,19 +61,7 @@ class App extends Component {
               path="/home"
               component={UserPage}
             />
-
-            {/* <ProtectedRoute
-              exact
-              path="/shelf"
-              component={ViewShelfList}
-            /> */}
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
-            {/* <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            /> */}
+            
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
           </Switch>
