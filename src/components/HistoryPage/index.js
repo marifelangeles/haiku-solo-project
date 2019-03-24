f71263af93b36252e435a1064c005538f65b1cc8
Marifel Angeles <mmarifel@gmail.com>
2019-03-23 23:43:08 -0500

    style haiku cards in history

--- src/components/HistoryPage/index.js
+++ src/components/HistoryPage/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import HaikuList from './HaikuList';
-
-
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { CardHeader } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ShareIcon from '@material-ui/icons/Share';
 
 
 class HistoryPage extends Component {
@@ -17,6 +22,7 @@ class HistoryPage extends Component {
         this.props.dispatch({ type: 'GET_HAIKU'});
     }
 
+    // delete selected haiku
     handleDeleteClick = (id) => {
         console.log('in handleDeleteClick', id);
         this.props.dispatch({
@@ -31,22 +37,46 @@ class HistoryPage extends Component {
             <div>
                 <h2>My Haikus</h2>
                 {/* {JSON.stringify(this.props.haikuList)} */}
-                <ul>
+                <div>
                     {this.props.haikuList.map(haiku =>
-                        <li key={haiku.id}>
-                            <div>
-                                <h3>{haiku.word}</h3>
-                                <h4>{haiku.date}</h4>
-                                <div>
-                                    <p>{haiku.line1}</p>
-                                    <p>{haiku.line2}</p>
-                                    <p>{haiku.line3}</p>
-                                </div>
-                                <button onClick={ () => this.handleDeleteClick(haiku.id)}>Delete</button>
-                            </div>
-                        </li>
+                        <Card 
+                            key={haiku.id}
+                            style={{maxWidth: '375px'}}
+                        >
+                            <CardHeader 
+                                title={haiku.word}
+                                subheader={haiku.date}
+                            />
+                            <CardContent>
+                                <Typography variant="body1" gutterBottom>{haiku.line1}</Typography>
+                                <Typography variant="body1" gutterBottom>{haiku.line2}</Typography>
+                                <Typography variant="body1" gutterBottom>{haiku.line3}</Typography>
+                            </CardContent>
+                            {/* <CardContent>
+                                <Typography>{haiku.word}</Typography>
+                                <Typography>{haiku.date}</Typography>
+                            </CardContent> */}
+                                
+                            <CardActions>
+                                <IconButton 
+                                    aria-label="Delete"
+                                    onClick={() => this.handleDeleteClick(haiku.id)}
+                                >
+                                    <DeleteIcon />
+                                </IconButton>
+                                <IconButton aria-label="Share">
+                                    <ShareIcon />
+                                </IconButton>
+                                {/* <Button size="small"
+                                    onClick={() => this.handleDeleteClick(haiku.id)}
+                                >
+                                    Delete
+                                </Button> */}
+                            </CardActions>
+                                
+                        </Card>
                     )}
-                </ul>
+                </div>
 
 
             </div>
