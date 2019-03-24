6b767e1895766bcc05e152d26a9349631b6a53e5
Marifel Angeles <mmarifel@gmail.com>
2019-03-24 00:00:04 -0500

    style history with grid

--- src/components/HistoryPage/index.js
+++ src/components/HistoryPage/index.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { CardHeader } from '@material-ui/core';
+import { CardHeader, Grid } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ShareIcon from '@material-ui/icons/Share';
@@ -37,48 +37,37 @@ class HistoryPage extends Component {
             <div>
                 <h2>My Haikus</h2>
                 {/* {JSON.stringify(this.props.haikuList)} */}
-                <div>
+                <Grid
+                    container
+                    alignItems="center"
+                >
                     {this.props.haikuList.map(haiku =>
-                        <Card 
-                            key={haiku.id}
-                            style={{maxWidth: '375px'}}
-                        >
-                            <CardHeader 
-                                title={haiku.word}
-                                subheader={haiku.date}
-                            />
-                            <CardContent>
-                                <Typography variant="body1" gutterBottom>{haiku.line1}</Typography>
-                                <Typography variant="body1" gutterBottom>{haiku.line2}</Typography>
-                                <Typography variant="body1" gutterBottom>{haiku.line3}</Typography>
-                            </CardContent>
-                            {/* <CardContent>
-                                <Typography>{haiku.word}</Typography>
-                                <Typography>{haiku.date}</Typography>
-                            </CardContent> */}
-                                
-                            <CardActions>
-                                <IconButton 
-                                    aria-label="Delete"
-                                    onClick={() => this.handleDeleteClick(haiku.id)}
-                                >
-                                    <DeleteIcon />
-                                </IconButton>
-                                <IconButton aria-label="Share">
-                                    <ShareIcon />
-                                </IconButton>
-                                {/* <Button size="small"
-                                    onClick={() => this.handleDeleteClick(haiku.id)}
-                                >
-                                    Delete
-                                </Button> */}
-                            </CardActions>
-                                
-                        </Card>
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={haiku.id}>
+                            <Card style={{height: '250px'}}>
+                                <CardHeader 
+                                    title={haiku.word}
+                                    subheader={haiku.date}
+                                />
+                                <CardContent>
+                                    <Typography variant="body1" gutterBottom>{haiku.line1}</Typography>
+                                    <Typography variant="body1" gutterBottom>{haiku.line2}</Typography>
+                                    <Typography variant="body1" gutterBottom>{haiku.line3}</Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <IconButton 
+                                        aria-label="Delete"
+                                        onClick={() => this.handleDeleteClick(haiku.id)}
+                                    >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="Share">
+                                        <ShareIcon />
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
+                        </Grid>
                     )}
-                </div>
-
-
+                </Grid>
             </div>
         );
     }
