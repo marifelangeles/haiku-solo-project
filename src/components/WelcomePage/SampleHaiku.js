6f6a95416673fd335a40008dd118b44c3c82a788
Marifel Angeles <mmarifel@gmail.com>
2019-05-19 10:53:29 -0500

    animate sample haiku

--- src/components/WelcomePage/SampleHaiku.js
+++ src/components/WelcomePage/SampleHaiku.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Typography from '@material-ui/core/Typography';
-
+import './welcomePage.css';
 
 
 
@@ -9,10 +8,13 @@ class SampleHaiku extends Component {
 
     render() {
         return (
-            <div style={{ marginTop: '4rem', marginBottom: '10rem', padding: '1rem'}}>
-                <Typography variant="h6" gutterBottom>Relax, let it go</Typography>
+            <div className="css-typing" >
+                {/* <Typography variant="h6" gutterBottom>Relax, let it go</Typography>
                 <Typography variant="h6" gutterBottom>Let creativity flow</Typography>
-                <Typography variant="h6" gutterBottom>Start with a word and ... </Typography>
+                <Typography variant="h6" gutterBottom>Start with a word and ... </Typography> */}
+                <p>Relax, let it go</p>
+                <p>Let creativity flow</p>
+                <p>And embrace constraints</p>
             </div>
         );
     }
